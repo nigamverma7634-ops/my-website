d054a11c3482f2941dd6aa668147dda37d12521f
nigamverma7634-ops <nigamverma7634@gmail.com>
2026-01-03 17:38:21 +0530

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,36 @@
+let posts = [];
+
+function renderPosts() {
+  const box = document.getElementById("posts");
+  box.innerHTML = "";
+
+  posts.forEach((post, index) => {
+    box.innerHTML += `
+      <h4>${post.title}</h4>
+      <p>${post.content}</p>
+      <button onclick="deletePost(${index})">Delete</button>
+      <hr>
+    `;
+  });
+}
+
+function addPost() {
+  const title = document.getElementById("title").value;
+  const content = document.getElementById("content").value;
+
+  if (!title || !content) {
+    alert("Title aur content likho");
+    return;
+  }
+
+  posts.push({ title, content });
+  renderPosts();
+
+  document.getElementById("title").value = "";
+  document.getElementById("content").value = "";
+}
+
+function deletePost(index) {
+  posts.splice(index, 1);
+  renderPosts();
+}
