4c0ee60f156316cf179fe870e38aa91a5ac795b1
nigamverma7634-ops <nigamverma7634@gmail.com>
2026-01-06 09:11:48 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -2,8 +2,8 @@
 // SUPABASE CONFIG (ONLY EDIT THESE 2 LINES)
 // ===============================
 
-const SUPABASE_URL = "PASTE_YOUR_PROJECT_URL_HERE";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_ANON_PUBLIC_KEY_HERE";
+const SUPABASE_URL = "https://cjoupniubbkwhmlbzfek.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqb3Vwbml1YmJrd2htbGJ6ZmVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc1MjE1NDQsImV4cCI6MjA4MzA5NzU0NH0.DkGna34dS0v63WtPQtoDSs61tEAlh-vlo3ZQ6iccWyg";
 
 // ===============================
 // DO NOT TOUCH BELOW CODE
