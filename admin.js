220273cb06eb1c87ac5d44dd237051eec5eeccfb
nigamverma7634-ops <nigamverma7634@gmail.com>
2026-01-06 08:22:37 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,22 +1,3 @@
-async function loginAdmin() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  if (!email || !password) {
-    alert("Email aur password required hai");
-    return;
-  }
-
-  const { data, error } = await supabaseClient.auth.signInWithPassword({
-    email: email,
-    password: password
-  });
-
-  if (error) {
-    alert("Login failed: " + error.message);
-    console.error(error);
-  } else {
-    // ✅ Login success → dashboard
-    window.location.href = "dashboard.html";
-  }
+function loginAdmin() {
+  alert("admin.js load ho gaya ✅");
 }
